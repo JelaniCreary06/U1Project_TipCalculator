55c6e41c22dfff24b168125764a170eb63c7fb4d
BT_4N2_29 <Jcreary7087@bths.edu>
2022-09-21 12:23:01 -0400

    Finished project.

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -11,13 +11,13 @@ public class TipCalculator {
 
 
         System.out.println("Are you now leaving?\nYou'll receive a few prompts for payment options.");
-        System.out.print("Enter the total bill (payment): ");
+        System.out.print("Enter the total bill (total due): ");
         bill = input.nextDouble();
 
-        System.out.print("Enter the tip amount (as a whole number): ");
-        tip = (double) (input.nextInt() / 100);
+        System.out.print("Enter the tip percent (as a whole number): ");
+        tip = (input.nextDouble() / 100);
 
-        System.out.print("\nThe total amount of people to split between: ");
+        System.out.print("The total amount of people to split between: ");
         people = input.nextInt();
 
         totalTip = bill * tip;
@@ -25,8 +25,14 @@ public class TipCalculator {
         tipPerPerson = totalTip / people;
         totalPerPerson = totalBill / people;
 
+        totalTip = Math.round(totalTip * 100);
+        totalBill = Math.round(totalBill * 100);
+        tipPerPerson = Math.round(tipPerPerson * 100);
+        totalPerPerson = Math.round(totalPerPerson * 100);
+
+        totalTip /= 100; totalBill /= 100; tipPerPerson /= 100; totalPerPerson /= 100;
 
 
-
+        System.out.println("\nCalculations complete.\nTotal Tip Amount: " + totalTip + "\nTotal Bill (includimg tip): " + totalBill + "\nTip Per Person: " + tipPerPerson + "\nTotal Per Person: " + totalPerPerson);
     }
 }
