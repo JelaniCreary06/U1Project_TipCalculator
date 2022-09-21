a1b9bca3197b46d994526376318346c10de45dfd
BT_4N2_29 <Jcreary7087@bths.edu>
2022-09-21 12:32:44 -0400

    Added comments.

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class TipCalculator {
     public static void main(String[] args) {
@@ -8,8 +9,9 @@ public class TipCalculator {
         int people;
 
         Scanner input = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
 
-
+        //Lines 15 - 23 get the inputted values.
         System.out.println("Are you now leaving?\nYou'll receive a few prompts for payment options.");
         System.out.print("Enter the total bill (total due): ");
         bill = input.nextDouble();
@@ -20,19 +22,21 @@ public class TipCalculator {
         System.out.print("The total amount of people to split between: ");
         people = input.nextInt();
 
+        //Lines 25 - 29 calculate the appropriate values.
         totalTip = bill * tip;
         totalBill = bill + totalTip;
         tipPerPerson = totalTip / people;
         totalPerPerson = totalBill / people;
 
+        // Lines 28 - 34 round the numbers to the nearest decimal places. there'll always be 2 decimal places.
         totalTip = Math.round(totalTip * 100);
         totalBill = Math.round(totalBill * 100);
         tipPerPerson = Math.round(tipPerPerson * 100);
         totalPerPerson = Math.round(totalPerPerson * 100);
 
-        totalTip /= 100; totalBill /= 100; tipPerPerson /= 100; totalPerPerson /= 100;
+        df.format(totalTip /= 100); df.format(totalBill /= 100); df.format(tipPerPerson /= 100); df.format(totalPerPerson /= 100);
 
-
+        //The following print statement prints out the completed calculations.
         System.out.println("\nCalculations complete.\nTotal Tip Amount: " + totalTip + "\nTotal Bill (includimg tip): " + totalBill + "\nTip Per Person: " + tipPerPerson + "\nTotal Per Person: " + totalPerPerson);
     }
 }
