7a918ca7cce4b5f06a128668dc24df9a9eda5f8c
Antwi <antwinecreary@gmail.com>
2022-09-21 20:48:35 -0400

    Updated rounding using decimal format, added comments in code.

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -11,7 +11,7 @@ public class TipCalculator {
         Scanner input = new Scanner(System.in);
         DecimalFormat df = new DecimalFormat("0.00");
 
-        //Lines 15 - 23 get the inputted values.
+        //Lines 15 - 23 get the inputted values through the Scanner method, and assign them to variables.
         System.out.println("Are you now leaving?\nYou'll receive a few prompts for payment options.");
         System.out.print("Enter the total bill (total due): ");
         bill = input.nextDouble();
@@ -22,21 +22,15 @@ public class TipCalculator {
         System.out.print("The total amount of people to split between: ");
         people = input.nextInt();
 
-        //Lines 25 - 29 calculate the appropriate values.
+        //Lines 25 - 29 calculate the appropriate values, and reasign them to defined variables.
         totalTip = bill * tip;
         totalBill = bill + totalTip;
         tipPerPerson = totalTip / people;
         totalPerPerson = totalBill / people;
 
-        // Lines 28 - 34 round the numbers to the nearest decimal places. there'll always be 2 decimal places.
-        totalTip = Math.round(totalTip * 100);
-        totalBill = Math.round(totalBill * 100);
-        tipPerPerson = Math.round(tipPerPerson * 100);
-        totalPerPerson = Math.round(totalPerPerson * 100);
-
-        df.format(totalTip /= 100); df.format(totalBill /= 100); df.format(tipPerPerson /= 100); df.format(totalPerPerson /= 100);
-
-        //The following print statement prints out the completed calculations.
-        System.out.println("\nCalculations complete.\nTotal Tip Amount: " + totalTip + "\nTotal Bill (includimg tip): " + totalBill + "\nTip Per Person: " + tipPerPerson + "\nTotal Per Person: " + totalPerPerson);
+        /*The following print statement prints out the completed calculations,
+        while using decimal format to round to the first 2 decimal places.
+        */
+        System.out.println("\nCalculations complete.\nTotal Tip Amount: " + df.format(totalTip) + "\nTotal Bill (including tip): " + df.format(totalBill) + "\nTip Per Person: " + df.format(tipPerPerson) + "\nTotal Per Person: " + df.format(totalPerPerson));
     }
 }
